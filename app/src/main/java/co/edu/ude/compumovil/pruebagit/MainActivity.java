dab3e2dcc8ee57633b13767bfcf2cc84015dfca3
AndyVerona123 <andres.carrascal@udea.edu.co>
2018-08-29 20:12:50 -0500

    Este es un comit nuevo

--- app/src/main/java/co/edu/ude/compumovil/pruebagit/MainActivity.java
+++ app/src/main/java/co/edu/ude/compumovil/pruebagit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Hola mundo
         //Segundo mensaje
+        //Comentario 2
     }
 }
