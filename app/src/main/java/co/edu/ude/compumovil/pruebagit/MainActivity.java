cdcc1969eea6eebab463b53181aec5593f308be4
AndyVerona123 <andres.carrascal@udea.edu.co>
2018-08-29 20:10:13 -0500

    Commit inicial

---
+++ app/src/main/java/co/edu/ude/compumovil/pruebagit/MainActivity.java
@@ -0,0 +1,14 @@
+package co.edu.ude.compumovil.pruebagit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hola mundo
+    }
+}
